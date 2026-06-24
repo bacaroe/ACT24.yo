8737f852862cbc5d9cc3fb75e8dd13a2e89e1dd5
Bastián Caro <ba.caroe@duocuc.cl>
2026-06-24 12:22:14 -0400

    Commit v2 Registrar

---
+++ funciones.py
@@ -0,0 +1,30 @@
+
+#Colección para alamacenar temporalmente los personajes
+personajes = []
+
+def buscar(nombre):
+    for i in range(len(personajes)):
+        if personajes[i]["nombre"]==nombre:
+            return i #retornamos la posición donde está 
+    return -1 #Si el personaje no se encuentra es -1
+
+def agregar(nombre,clase,nivel):
+    #Validar datos  
+    if len(nombre.strip())==0 or len(nombre.strip())>20:
+        print("Nombre no válido")
+        return 
+    elif buscar(nombre)>=0:
+        print("Nombre ya existe")
+        return
+    elif clase not in ("Guerrero", "Mago", "Picaro"):
+        print("Clase no válida, debe ser Guerrero, Mago o Pícaro")
+        return  
+    elif nivel<=0 or nivel>50:
+        print("El nivel debe estar entre 1 y 50")
+        return
+    #Registar personaje en la lista
+    rango = "Recluta"
+    if nivel>=30: rango = "Élite"
+    pj = {"nombre":nombre,"clase":clase,"nivel":nivel,"rango":rango}
+    personajes.append(pj)
+    print("Personaje registrado")
